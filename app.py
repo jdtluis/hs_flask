f77e82242763c32f6a5553fa42ce7e893809783e
jdtluis <83558495+jdtluis@users.noreply.github.com>
2023-01-25 16:35:36 -0300

    Adding req

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from app.main import app
 
 if __name__ == "__main__":
-	app.run(debug = False, passthrough_errors=True) #, host='0.0.0.0', port=8080
+	# app.run(debug = False, passthrough_errors=True) #, host='0.0.0.0', port=8080
